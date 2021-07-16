2ad6cf127aba5dc02981ff1e1083671dcce0f7e9
Vayugundla Girish Guptha <girishvayugundla@gmail.com>
2021-07-16 23:29:29 +0530

    Unorderd List problem

---
+++ DataStructureProblems/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DataStructureProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Data Structure Problems");
+            Console.WriteLine("1.Unordered List");
+            Console.WriteLine("enter your option :");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    UnorderdList.List();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ DataStructureProblems/UnorderdList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace DataStructureProblems
+{
+    public class UnorderdList
+    {
+        public static void List()
+        {
+            string text = File.ReadAllText(@"C:\Users\giris\source\repos\DataStructureProblems\DataStructureProblems\DS.txt");
+            string[] textArray = text.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            CustomLinkedList<string> list = new CustomLinkedList<string>();
+            for (int i = textArray.Length - 1; i >= 0; i--)
+            {
+                list.InsertFront(textArray[i]);
+            }
+            list.Display();
+            Console.WriteLine("\nEnter a string which you want to search");
+            string searchStr = Console.ReadLine();
+            if (list.Search(searchStr))
+            {
+                list.DeleteElement(searchStr);
+            }
+            else
+            {
+                list.InsertFront(searchStr);
+            }
+            string textFile = list.ReturnString();
+            File.WriteAllText(@"C:\Users\giris\source\repos\DataStructureProblems\DataStructureProblems\DS.txt", textFile);
+            list.Display();
+        }
+    }
+}
